6391ddb77c4612817ff2ae1258e6a86a869b8eaa
Doni Rubiagatra <rubiagatra@gmail.com>
2025-04-17 08:57:27 +0700

    learn function plug

--- lib/heads_up_web/router.ex
+++ lib/heads_up_web/router.ex
@@ -8,10 +8,16 @@ defmodule HeadsUpWeb.Router do
     plug :put_root_layout, html: {HeadsUpWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :snoop
   end
 
   pipeline :api do
     plug :accepts, ["json"]
+  end
+
+  def snoop(conn, _opts) do
+    answer = ~w(Yes No Maybe) |> Enum.random()
+    conn = assign(conn, :answer, answer)
   end
 
   scope "/", HeadsUpWeb do
@@ -21,9 +27,9 @@ defmodule HeadsUpWeb.Router do
   end
 
   # Other scopes may use custom stacks.
-  # scope "/api", HeadsUpWeb do
-  #   pipe_through :api
-  # end
+  scope "/api", HeadsUpWeb do
+    pipe_through :api
+  end
 
   # Enable LiveDashboard and Swoosh mailbox preview in development
   if Application.compile_env(:heads_up, :dev_routes) do
